ac7ca06314c3c7492c06e5b941e6517790e7e802
Dark <darkgrouptw@gmail.com>
2018-09-25 20:50:25 +0800

    更新新的 Project

--- DentistTrainningDataMaker/main.cpp
+++ DentistTrainningDataMaker/main.cpp
@@ -1,7 +1,18 @@
-/*
-這個主要產生 Trainning Data 用的
+﻿/*
+這邊是要從 Raw Data 產生資料
+
+ConvertList.txt
+檔案目錄
+沒蓋肉 有蓋肉
+沒蓋肉 有蓋肉
+沒蓋肉 有蓋肉
+.
+.
+.
 */
-#include <QFile>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
