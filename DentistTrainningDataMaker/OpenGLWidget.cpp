8b5c6fea29421f9e998b757b63f38f63f3c4be64
Dark <darkgrouptw@gmail.com>
2018-10-09 14:52:40 +0800

    1. 加上滑鼠，可以順利移動的 功能 2. 畫出 Edge

--- DentistTrainningDataMaker/OpenGLWidget.cpp
+++ DentistTrainningDataMaker/OpenGLWidget.cpp
@@ -13,59 +13,99 @@ void OpenGLWidget::initializeGL()
 	glClearColor(0.5f, 0.5, 0.5f, 1);
 	glEnable(GL_DEPTH_TEST);
 
-
 	CalcMatrix();
+}
+void OpenGLWidget::paintGL()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glLoadMatrixf((ProjectionMatrix * ViewMatrix).data());
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+
+	DrawGround();
+	DrawSTL();
 }
-void OpenGLWidget::paintGL()
+
+// 滑鼠事件
+void OpenGLWidget::mousePressEvent(QMouseEvent *e)
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	DrawGrid();
+	PressPoint = e->pos();
+	TempArcAngle = ArcAngle;
+}
+void OpenGLWidget::mouseMoveEvent(QMouseEvent *e)
+{
+	CurrentPoint = e->pos();
 
+	int width = CurrentPoint.x() - PressPoint.x();
+	float rate = (float)width / this->width();
+
+	ArcAngle = TempArcAngle + rate * 180;
+	cout << ArcAngle << endl;
+	CalcMatrix();
+
+	this->update();
 }
 
+//////////////////////////////////////////////////////////////////////////
 // 外部呼叫函數
+//////////////////////////////////////////////////////////////////////////
 bool OpenGLWidget::LoadSTLFile(QString FileName)
 {
 	if (!OpenMesh::IO::read_mesh(STLFile, FileName.toStdString()))
 	{
 		cout << "讀取錯誤!!" << endl;
+		IsLoaded = false;
 		return false;
 	}
-	cout << "模型點數：" << STLFile.n_faces() << endl;
+	cout << "模型面數：" << STLFile.n_faces() << endl;
+	IsLoaded = true;
 	return true;
 }
 
+//////////////////////////////////////////////////////////////////////////
 // 矩陣相關
+//////////////////////////////////////////////////////////////////////////
 void OpenGLWidget::CalcMatrix()
 {
 	float AngleInRadian = ElevationAngle * M_PI / 180;
-	float GoundRadius = Radius * cos(AngleInRadian);
+	float GroundRadius = Radius * cos(AngleInRadian);
 	float Height = Radius * sin(AngleInRadian);
 
 	float ArcAngleInRadian = ArcAngle * M_PI / 180;
-	float GoundX = GoundRadius * cos(ArcAngleInRadian);
-	float GoundZ = GoundRadius * sin(ArcAngleInRadian);
+	float GroundX = GroundRadius * cos(ArcAngleInRadian);
+	float GroundZ = GroundRadius * sin(ArcAngleInRadian);
 
 	ProjectionMatrix.setToIdentity();
 	ProjectionMatrix.perspective(60, 1, 0.1f, 100);
 
 	ViewMatrix.setToIdentity();
 	ViewMatrix.lookAt(
-		QVector3D(GoundX, Height, GoundZ),
+		QVector3D(GroundX, Height, GroundZ),
 		QVector3D(0, 0, 0),
 		QVector3D(0, 1, 0)
 	);
 }
 
-void OpenGLWidget::DrawGrid()
+//////////////////////////////////////////////////////////////////////////
+// 畫畫的 Function
+//////////////////////////////////////////////////////////////////////////
+void OpenGLWidget::DrawGround()
 {
+	#pragma region 地板
+	float lowerY = -0.01f;
+	glColor4f(0.7f, 0.7f, 0.7f, 1);
+	glBegin(GL_QUADS);
+	glVertex3f(GridMax.x(), lowerY, GridMax.y());
+	glVertex3f(GridMin.x(), lowerY, GridMax.y());
+	glVertex3f(GridMin.x(), lowerY, GridMin.y());
+	glVertex3f(GridMax.x(), lowerY, GridMin.y());
+	glEnd();
+	#pragma endregion
+
 	glColor4f(1.f, 1.f, 1.f, 1.f);
 	glBegin(GL_LINES);
 	#pragma region 橫
@@ -83,4 +123,35 @@ void OpenGLWidget::DrawGrid()
 	}
 	#pragma endregion
 	glEnd();
+}
+void OpenGLWidget::DrawSTL()
+{
+	#pragma region 先判斷是不是空的
+	if (!IsLoaded)
+		return;
+	#pragma endregion
+	#pragma region 跑每一個點，把它畫出來 (暫時先用 OpenGL 1 來畫)
+	glColor4f(0.968f, 0.863f, 0.445f, 1);					// #F7DD72
+	glBegin(GL_TRIANGLES);
+	for (MeshType::FaceIter f_iter = STLFile.faces_begin(); f_iter != STLFile.faces_end(); f_iter++)
+		for (MeshType::FaceVertexIter fv_iter = STLFile.fv_iter(f_iter); fv_iter.is_valid(); fv_iter++)
+		{
+			MeshType::Point p = STLFile.point(fv_iter);
+			glVertex3f(p[0], p[1], p[2]);
+		}
+	glEnd();
+
+	glColor4f(0, 0, 0, 1);
+	glBegin(GL_LINES);
+	for (MeshType::FaceIter f_iter = STLFile.faces_begin(); f_iter != STLFile.faces_end(); f_iter++)
+		for (MeshType::FaceEdgeIter fe_iter = STLFile.fe_iter(f_iter); fe_iter.is_valid(); fe_iter++)
+		{
+			
+			MeshType::Point FirstP = STLFile.point(STLFile.to_vertex_handle(STLFile.halfedge_handle(fe_iter, 0)));
+			MeshType::Point SecondP = STLFile.point(STLFile.to_vertex_handle(STLFile.halfedge_handle(fe_iter, 1)));
+			glVertex3f(FirstP[0], FirstP[1], FirstP[2]);
+			glVertex3f(SecondP[0], SecondP[1], SecondP[2]);
+		}
+	glEnd();
+	#pragma endregion
 }
--- DentistTrainningDataMaker/OpenGLWidget.h
+++ DentistTrainningDataMaker/OpenGLWidget.h
@@ -2,16 +2,18 @@
 #include <iostream>
 #include <cmath>
 
+#include <QPoint>
 #include <QVector2D>
 #include <QVector3D>
 #include <QMatrix4x4>
+#include <QMouseEvent>
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions_4_5_Core>
 
 #include "OpenMesh/Core/IO/MeshIO.hh"
 #include "OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh"
 
-typedef OpenMesh::TriMesh_ArrayKernelT<> Mesh;
+typedef OpenMesh::TriMesh_ArrayKernelT<> MeshType;
 
 using namespace std;
 
@@ -24,6 +26,9 @@ public:
 	void initializeGL();
 	void paintGL();
 
+	// 滑鼠事件
+	void mousePressEvent(QMouseEvent *);
+	void mouseMoveEvent(QMouseEvent *);
 
 	// Connection Funciton
 	bool LoadSTLFile(QString);
@@ -32,20 +37,28 @@ private:
 	void CalcMatrix();						// 重算矩陣
 
 	#pragma region 畫畫 Function
-	void DrawGrid();
+	void DrawGround();
+	void DrawSTL();
 
 	QVector2D GridMin = QVector2D(-10, -10);
 	QVector2D GridMax = QVector2D(10, 10);
 
 	// MVP 矩陣
-	QMatrix4x4 ProjectionMatrix;
-	QMatrix4x4 ViewMatrix;
+	QMatrix4x4		ProjectionMatrix;
+	QMatrix4x4		ViewMatrix;
 
 	const float		ElevationAngle = 30;	// 仰角
-	const float		Radius = 15;			// 半徑
+	const float		Radius = 30;			// 半徑
 	int				ArcAngle = 0;			// 角度
-	#pragma endregion
+	int				TempArcAngle = 0;		// 暫存角度 (For 滑鼠滑動使用)
 
-	Mesh STLFile;
+	// Mesh
+	MeshType			STLFile;
+	bool			IsLoaded = false;
+	#pragma endregion
+	#pragma region 拖移
+	QPoint			PressPoint;
+	QPoint			CurrentPoint;
+	#pragma endregion
 };
 
