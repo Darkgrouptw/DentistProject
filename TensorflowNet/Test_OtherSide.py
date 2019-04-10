3cce74049955eb216930e15c110e94f789bbc7f2
Dark <darkgrouptw@gmail.com>
2019-04-10 19:09:36 +0800

    1. 刪除之前留下來的垃圾 2. 新增 Test_Python & Test_DentistDLL 的兩個測試

---
+++ TensorflowNet/Test_OtherSide.py
@@ -0,0 +1,59 @@
+from Network.Network_Prob import Network_Prob
+from DataManager import DataManager_OtherSide
+import os
+import cv2
+
+# 參數
+lr = 5e-3
+kernelSize = 5
+
+# 資料集
+InputFileList = []
+LabeledFileList = []
+# Windows => E:/DentistData/NetworkData
+# Ubuntu => /home/Dark/NetworkData
+DataPath = [
+    "E:/DentistData/NetworkData/2019.01.08 ToothBone1",
+    "E:/DentistData/NetworkData/2019.01.08 ToothBone2",
+    "E:/DentistData/NetworkData/2019.01.08 ToothBone3.1",
+    "E:/DentistData/NetworkData/2019.01.08 ToothBone3.2",
+    "E:/DentistData/NetworkData/2019.01.08 ToothBone7.1",
+    "E:/DentistData/NetworkData/2019.01.08 ToothBone8.1",
+    "E:/DentistData/NetworkData/2019.01.08 ToothBone9.1",
+    "E:/DentistData/NetworkData/2019.03.05 ToothBone1_slim",
+    "E:/DentistData/NetworkData/2019.03.05 ToothBone2_slim",
+    "E:/DentistData/NetworkData/2019.03.05 ToothBone3_no_slim",
+    "E:/DentistData/NetworkData/2019.03.05 ToothBone4_slim",
+    "E:/DentistData/NetworkData/2019.03.05 ToothBone5_slim",
+    "E:/DentistData/NetworkData/2019.03.05 ToothBone6_slim",
+    "E:/DentistData/NetworkData/2019.03.05 ToothBone7_slim",
+    "E:/DentistData/NetworkData/2019.03.05 ToothBone8_slim"
+]
+
+for i in range(len(DataPath)):
+    tempInputPath = os.path.join(DataPath[i], "./OtherSide.png")
+    tempLabeledPath = os.path.join(DataPath[i], "./OtherSide_Label.png")
+
+    # 加進陣列裡
+    InputFileList.append(tempInputPath)
+    LabeledFileList.append(tempLabeledPath)
+DM = DataManager_OtherSide.DataManager(InputFileList, LabeledFileList, 1, 101)
+
+# Network
+logDir = "./logs/"
+# net = Network3D.Network3D(40, 40, 40, 2, lr, kernelSize, logDir, False)
+net = Network_Prob(101, 101, 1, lr, kernelSize, logDir, False)
+
+# 讀資料
+net.LoadWeight("./logs/0.005/kernel_5")
+
+ValidData = DM.TestFirstNBoxOfTrainData(len(DataPath))
+predictData = net.Predict(ValidData)
+predictData = predictData.reshape([-1, 250, 250])        # 轉成圖
+print("Test Image Shape:", predictData.shape)
+
+# 存出來
+for i in range(predictData.shape[0]):
+    cv2.imwrite("D:/" + str(i) + ".png", predictData[i] * 255)
+net.Release()
+
---
+++ Test_DentistDLL/main.cpp
@@ -0,0 +1,43 @@
+﻿/*
+這個主要是用來測試
+DLL 能不能 Work
+*/
+#include <QtCore/QCoreApplication>
+#include <iostream>
+#include <windows.h>
+#include <msclr\marshal_cppstd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	#pragma region OCT Test DLL 的部分
+	unsigned short IntData = 6;
+	unsigned short OutData = 0;
+	cli::array<unsigned short> ^IntArray;
+	System::String ^TestString;
+
+	//cout << "現在目錄：" << QDir::currentPath().toStdString() << endl;
+	OCTTest::OCTTest::t1(IntData, OutData, IntArray, TestString);
+	cout << IntData << endl;
+	cout << OutData << endl;
+
+	for (int i = 0; i< IntArray->Length; i++)
+	{
+		cout << IntArray[i] << " ";
+	}
+	cout << endl;
+	std::string unmanaged = msclr::interop::marshal_as<std::string>(TestString);
+	cout << unmanaged << endl;
+	#pragma endregion
+	#pragma region OCT 64 Test
+	/*int DeviceID = -1;
+	OCT64::OCT64::Init(
+		4,
+		DeviceID
+	);
+	cout << "DeviceID" << DeviceID << endl;*/
+	#pragma endregion
+	system("PAUSE");
+	return 0;
+}
