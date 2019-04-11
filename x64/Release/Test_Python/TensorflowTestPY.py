9ba772f0887be3e7551a93ba4b5e0481c891e5e6
Dark <darkgrouptw@gmail.com>
2019-04-11 11:00:14 +0800

    更新 Python Test py

---
+++ x64/Release/Test_Python/TensorflowTestPY.py
@@ -0,0 +1,23 @@
+﻿import tensorflow as tf
+
+# 先測試 Tensorflow 的變數
+TensorA = tf.placeholder(tf.int32, 1, "TensorA")
+TensorB = tf.placeholder(tf.int32, 1, "TensorB")
+TensorC = tf.add(TensorA, TensorB)
+Sess = tf.Session()
+
+# 測試
+def TestPrint():
+    print("TestPY")
+def AddTest(a, b):
+    return a + b
+def TensorTest(a, b):
+    # global TensorA, TensorB, TensorC, Sess
+    feed_dict = {
+        TensorA: [a],
+        TensorB: [b]
+    }
+    result = Sess.run(TensorC, feed_dict=feed_dict)
+    print("Tensor Add: ", result)
+
+print("Test2")
